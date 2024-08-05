83bb1fd91f546fc3d6d0faeca1a63ad42c392824
MohdSharik99 <livisliquoro999@gmail.com>
2024-08-05 19:41:39 +0530

    Pix2Pix_model_updated

--- Pix2Pix_model.py
+++ Pix2Pix_model.py
@@ -1,100 +1,81 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Conv2D, Conv2DTranspose, UpSampling2D, concatenate, BatchNormalization, LeakyReLU, ReLU
-from tensorflow.keras.models import Model
 import numpy as np
-import matplotlib.pyplot as plt
+import cv2
+import os
 import time
-
-
+import glob
+from tensorflow.keras.layers import Input, Conv2D, Conv2DTranspose, concatenate, BatchNormalization, LeakyReLU, ReLU
+from tensorflow.keras.models import Model
+import matplotlib.pyplot as plt
 
 # Define the downsampling and upsampling blocks
 def downsample(filters, size, apply_batchnorm=True):
     initializer = tf.random_normal_initializer(0., 0.02)
     result = tf.keras.Sequential()
-    result.add(
-        Conv2D(filters, size, strides=2, padding='same', kernel_initializer=initializer, use_bias=False))
-
+    result.add(tf.keras.layers.Conv2D(filters, size, strides=2, padding='same', kernel_initializer=initializer, use_bias=False))
     if apply_batchnorm:
-        result.add(BatchNormalization())
-
-    result.add(LeakyReLU())
-
+        result.add(tf.keras.layers.BatchNormalization())
+    result.add(tf.keras.layers.LeakyReLU())
     return result
 
 def upsample(filters, size, apply_dropout=False):
     initializer = tf.random_normal_initializer(0., 0.02)
     result = tf.keras.Sequential()
-    result.add(
-        Conv2DTranspose(filters, size, strides=2, padding='same', kernel_initializer=initializer, use_bias=False))
-
-    result.add(BatchNormalization())
-
+    result.add(tf.keras.layers.Conv2DTranspose(filters, size, strides=2, padding='same', kernel_initializer=initializer, use_bias=False))
+    result.add(tf.keras.layers.BatchNormalization())
     if apply_dropout:
         result.add(tf.keras.layers.Dropout(hyperparams['dropout_rate']))
-
-    result.add(ReLU())
-
+    result.add(tf.keras.layers.ReLU())
     return result
 
 # Define the Generator model
-def Generator():
-    inputs = Input(shape=[256, 256, 1])
+def Generator(hyperparams):
+    inputs = tf.keras.layers.Input(shape=hyperparams['input_shape'])
 
-    down_stack = []
-    for i in range(hyperparams['num_layers']):
-        filters = hyperparams['initial_filters'] * (2 ** i)
-        down_stack.append(downsample(filters, hyperparams['kernel_size'], apply_batchnorm=hyperparams['batch_norm']))
-
-    up_stack = []
-    for i in range(hyperparams['num_layers']-1, 0, -1):
-        filters = hyperparams['initial_filters'] * (2 ** i)
-        up_stack.append(upsample(filters, hyperparams['kernel_size'], apply_dropout=hyperparams['dropout']))
-
+    down_stack = [downsample(hyperparams['initial_filters'] * (2 ** i), hyperparams['kernel_size'], apply_batchnorm=hyperparams['batch_norm']) for i in range(hyperparams['num_layers'])]
+    up_stack = [upsample(hyperparams['initial_filters'] * (2 ** i), hyperparams['kernel_size'], apply_dropout=hyperparams['dropout']) for i in range(hyperparams['num_layers']-1, 0, -1)]
     initializer = tf.random_normal_initializer(0., 0.02)
-    last = Conv2DTranspose(2, hyperparams['kernel_size'], strides=2, padding='same', kernel_initializer=initializer, activation='tanh')
+    last = tf.keras.layers.Conv2DTranspose(3, hyperparams['kernel_size'], strides=2, padding='same', kernel_initializer=initializer, activation='tanh')
 
     x = inputs
-
-    # Downsampling through the model
     skips = []
     for down in down_stack:
         x = down(x)
         skips.append(x)
 
     skips = reversed(skips[:-1])
-
-    # Upsampling and establishing the skip connections
     for up, skip in zip(up_stack, skips):
         x = up(x)
-        x = concatenate([x, skip])
+        x = tf.keras.layers.concatenate([x, skip])
 
     x = last(x)
 
-    return Model(inputs=inputs, outputs=x)
+    return tf.keras.Model(inputs=inputs, outputs=x)
+
 
 # Define the Discriminator model
-def Discriminator():
+def Discriminator(hyperparams):
     initializer = tf.random_normal_initializer(0., 0.02)
+    
+    inp = tf.keras.layers.Input(shape=hyperparams['input_shape'], name='input_image')
+    tar = tf.keras.layers.Input(shape=[*hyperparams['input_shape'][:2], 3], name='target_image')
 
-    inp = Input(shape=[256, 256, 1], name='input_image')
-    tar = Input(shape=[256, 256, 2], name='target_image')
-
-    x = concatenate([inp, tar])
+    x = tf.keras.layers.concatenate([inp, tar])
 
     down1 = downsample(64, 4, False)(x)
     down2 = downsample(128, 4)(down1)
     down3 = downsample(256, 4)(down2)
 
     zero_pad1 = tf.keras.layers.ZeroPadding2D()(down3)
-    conv = Conv2D(512, 4, strides=1, kernel_initializer=initializer, use_bias=False)(zero_pad1)
+    conv = tf.keras.layers.Conv2D(512, 4, strides=1, kernel_initializer=initializer, use_bias=False)(zero_pad1)
 
-    batchnorm1 = BatchNormalization()(conv)
-    leaky_relu = LeakyReLU()(batchnorm1)
+    batchnorm1 = tf.keras.layers.BatchNormalization()(conv)
+    leaky_relu = tf.keras.layers.LeakyReLU()(batchnorm1)
 
     zero_pad2 = tf.keras.layers.ZeroPadding2D()(leaky_relu)
-    last = Conv2D(1, 4, strides=1, kernel_initializer=initializer)(zero_pad2)
+    last = tf.keras.layers.Conv2D(1, 4, strides=1, kernel_initializer=initializer)(zero_pad2)
 
-    return Model(inputs=[inp, tar], outputs=last)
+    return tf.keras.Model(inputs=[inp, tar], outputs=last)
 
 # Loss functions
 loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
@@ -110,6 +91,12 @@ def discriminator_loss(disc_real_output, disc_generated_output):
     generated_loss = loss_object(tf.zeros_like(disc_generated_output), disc_generated_output)
     total_disc_loss = real_loss + generated_loss
     return total_disc_loss
+
+# PSNR metric
+def psnr_metric(y_true, y_pred):
+    max_pixel = 1.0
+    psnr_value = tf.image.psnr(y_true, y_pred, max_val=max_pixel)
+    return tf.reduce_mean(psnr_value)
 
 # Training step function
 def train_step(generator, discriminator, input_image, target):
@@ -130,25 +117,33 @@ def train_step(generator, discriminator, input_image, target):
     return gen_total_loss, disc_loss
 
 # Define the evaluation function
-def evaluate_model(dataset, generator):
+def evaluate_model(dataset, generator, discriminator):
     total_gen_loss = 0
+    total_psnr = 0
     num_batches = 0
     for input_image, target in dataset:
         gen_output = generator(input_image, training=False)
-        gen_total_loss, _, _ = generator_loss(None, gen_output, target)
+        disc_generated_output = discriminator([input_image, gen_output], training=False)
+        gen_total_loss, _, _ = generator_loss(disc_generated_output, gen_output, target)
         total_gen_loss += gen_total_loss
+
+        # Compute PSNR
+        psnr_value = psnr_metric(target, gen_output)
+        total_psnr += psnr_value
         num_batches += 1
     avg_gen_loss = total_gen_loss / num_batches
-    return avg_gen_loss
+    avg_psnr = total_psnr / num_batches
+    return avg_gen_loss, avg_psnr
 
 # Training function with validation loss
 def model_fit(train_ds, val_ds, hyperparams, checkpoint, checkpoint_prefix):
     gen_losses = []
     disc_losses = []
     val_gen_losses = []
+    val_psnrs = []
 
-    generator = Generator()
-    discriminator = Discriminator()
+    generator = Generator(hyperparams)
+    discriminator = Discriminator(hyperparams)
 
     # Define the optimizers
     generator_optimizer = tf.keras.optimizers.Adam(learning_rate=hyperparams['learning_rate'], beta_1=hyperparams['beta_1'])
@@ -162,56 +157,52 @@ def model_fit(train_ds, val_ds, hyperparams, checkpoint, checkpoint_prefix):
         # Progress bar
         progbar = tf.keras.utils.Progbar(len(train_ds), stateful_metrics=['loss'])
         
-        for input_image, target in train_ds:
+        for step, (input_image, target) in enumerate(train_ds):
             gen_total_loss, disc_loss = train_step(generator, discriminator, input_image, target)
             epoch_gen_loss += gen_total_loss
             epoch_disc_loss += disc_loss
 
             # Update progress bar
-            progbar.update(progbar.seen + 1, [('loss', gen_total_loss.numpy())])
+            progbar.update(step + 1, [('gen_loss', gen_total_loss), ('disc_loss', disc_loss)])
 
-        avg_gen_loss = epoch_gen_loss / len(train_ds)
-        avg_disc_loss = epoch_disc_loss / len(train_ds)
+        gen_losses.append(epoch_gen_loss / len(train_ds))
+        disc_losses.append(epoch_disc_loss / len(train_ds))
 
-        # Compute validation loss
-        avg_val_gen_loss = evaluate_model(val_ds, generator)
+        val_gen_loss, val_psnr = evaluate_model(val_ds, generator, discriminator)
+        val_gen_losses.append(val_gen_loss)
+        val_psnrs.append(val_psnr)
 
-        gen_losses.append(avg_gen_loss)
-        disc_losses.append(avg_disc_loss)
-        val_gen_losses.append(avg_val_gen_loss)
+        # Save checkpoint
+        checkpoint.save(file_prefix=checkpoint_prefix)
 
-        # Save checkpoints every 20 epochs
-        if (epoch + 1) % 20 == 0:
-            checkpoint.save(file_prefix=checkpoint_prefix)
+        print(f'Epoch {epoch+1}, Gen Loss: {gen_losses[-1]}, Disc Loss: {disc_losses[-1]}, Val Gen Loss: {val_gen_losses[-1]}, Val PSNR: {val_psnrs[-1]}, Time: {time.time() - start}')
 
-        print(f'Epoch: {epoch + 1}, Gen Loss: {avg_gen_loss.numpy()}, Disc Loss: {avg_disc_loss.numpy()}, Val Gen Loss: {avg_val_gen_loss.numpy()}')
-        print(f'Time taken for epoch {epoch + 1}: {time.time() - start} sec\n')
-
-    return gen_losses, disc_losses, val_gen_losses
+    return gen_losses, disc_losses, val_gen_losses, val_psnrs
 
 # Visualize losses
-def visualize_losses(gen_losses, disc_losses, val_gen_losses):
-    plt.figure(figsize=(15, 5))
+def visualize_losses(gen_losses, disc_losses, val_gen_losses, val_psnrs):
+    plt.figure(figsize=(12, 6))
     
-    plt.subplot(1, 3, 1)
-    plt.plot(gen_losses, label='Generator Loss (Train)')
+    # Plot Generator and Discriminator Losses
+    plt.subplot(1, 2, 1)
+    plt.plot(gen_losses, label='Generator Loss')
+    plt.plot(disc_losses, label='Discriminator Loss')
+    plt.plot(val_gen_losses, label='Validation Generator Loss')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
-    plt.title('Generator Loss over Time (Train)')
     plt.legend()
+    plt.title('Generator and Discriminator Losses')
 
-    plt.subplot(1, 3, 2)
-    plt.plot(disc_losses, label='Discriminator Loss (Train)')
+    # Plot Validation PSNR
+    plt.subplot(1, 2, 2)
+    plt.plot(val_psnrs, label='Validation PSNR')
     plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.title('Discriminator Loss over Time (Train)')
+    plt.ylabel('PSNR')
     plt.legend()
-    
-    plt.subplot(1, 3, 3)
-    plt.plot(val_gen_losses, label='Generator Loss (Val)')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.title('Generator Loss over Time (Val)')
-    plt.legend()
-    
+    plt.title('Validation PSNR')
+
+    plt.tight_layout()
     plt.show()
+
+
+   
--- Pix2Pix_train.py
+++ Pix2Pix_train.py
@@ -1,13 +1,16 @@
 import tensorflow as tf
 import numpy as np
-from PIL import Image
 import cv2
 import os
 import glob
+import time
 import matplotlib.pyplot as plt
-from tensorflow.keras.preprocessing.image import img_to_array, load_img
-from Data_processing_functions import *
+from PIL import Image
+from tensorflow.keras.layers import Input, Conv2D, Conv2DTranspose, concatenate, BatchNormalization, LeakyReLU, ReLU
+from tensorflow.keras.models import Model
 
+from Data_processing_functions import *
+from Pix2Pix_model import *
 
 # Define Data Directory
 dir_path = 'data'
@@ -21,26 +24,45 @@ black_images_paths = glob.glob(os.path.join(black_dir, '*.jpg'))
 # Create the original dataset
 dataset = create_dataset(black_images_paths, color_images_paths, target_size=(256, 256))
 
-# Create the dataset with combined grayscale and edge-detected images
-dataset_with_edges = create_dataset_with_edges(dataset)
+# Calculate the total number of elements in the dataset
+total_size = len(color_images_paths)
+train_size = int(total_size * 0.9)
+val_size = total_size - train_size
 
+# Shuffle the dataset
+dataset = dataset.shuffle(buffer_size=total_size)
 
-# Define hyperparameters
+# Split dataset into training and validation
+train_dataset = dataset.take(train_size)
+val_dataset = dataset.skip(train_size).take(val_size)
+
+# Example hyperparameters for testing
+# Hyperparameters
 hyperparams = {
+    'initial_filters': 64,
+    'kernel_size': 4,
+    'num_layers': 6,
+    'dropout_rate': 0.5,
+    'batch_norm': True,
+    'lambda_l1': 100,
     'learning_rate': 2e-4,
     'beta_1': 0.5,
     'batch_size': 1,
     'epochs': 50,
-    'dropout_rate': 0.5,
-    'lambda_l1': 100,
-    'batch_norm': True,
-    'initial_filters': 64,
-    'kernel_size': 4,
     'dropout': True,
-    'num_layers': 8,
-    'use_data_augmentation': True
+    'input_shape': (256, 256, 1)  # Input shape added
 }
 
+# Initialize models
+generator = Generator(hyperparams)
+discriminator = Discriminator(hyperparams)
+
+# Print model summaries
+print("Generator Summary:")
+generator.summary()
+
+print("\nDiscriminator Summary:")
+discriminator.summary()
 
 # Define the optimizers
 generator_optimizer = tf.keras.optimizers.Adam(learning_rate=hyperparams['learning_rate'], beta_1=hyperparams['beta_1'])
@@ -48,11 +70,8 @@ discriminator_optimizer = tf.keras.optimizers.Adam(learning_rate=hyperparams['le
 
 # Define the checkpoint directory
 checkpoint_dir = './training_checkpoints'
+os.makedirs(checkpoint_dir, exist_ok=True)
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-
-# Create model instances
-generator = Generator()
-discriminator = Discriminator()
 
 # Create a checkpoint object
 checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
@@ -61,69 +80,11 @@ checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
                                  discriminator=discriminator)
 
 # Train the model
-gen_losses, disc_losses, val_gen_losses = model_fit(dataset_with_edges, val_dataset_with_edges, 
-                                                    hyperparams, checkpoint, checkpoint_prefix)
+gen_losses, disc_losses, val_gen_losses, val_psnrs = model_fit(train_dataset, val_dataset, hyperparams, checkpoint, checkpoint_prefix)
 
+# Save the models
 generator.save('pix2pix_model_generator.h5')
 discriminator.save('pix2pix_model_discriminator.h5')
 
-
 # Visualize losses
-visualize_losses(gen_losses, disc_losses, val_gen_losses)
-
-
-
-# Load the generator model
-generator = tf.keras.models.load_model('pix2pix_model_generator.h5')
-
-# Load the discriminator model
-discriminator = tf.keras.models.load_model('pix2pix_model_discriminator.h5')
-
-# # Load and preprocess new sample data
-def load_and_preprocess_image(image_path, target_size=(256, 256), is_grayscale=True):
-    image = Image.open(image_path).convert('L' if is_grayscale else 'RGB')
-    image = image.resize(target_size)
-    image = np.array(image) / 255.0
-    if is_grayscale:
-        image = np.expand_dims(image, axis=-1)  # Add channel dimension for grayscale
-    return np.expand_dims(image, axis=0)  # Add batch dimension
-
-# Load the generator model
-generator = tf.keras.models.load_model('pix2pix_model_generator.h5')
-
-# Load new sample image
-new_image_path = black_images_paths[:10]
-new_image = load_and_preprocess_image(new_image_path, target_size=(256, 256), is_grayscale=True)
-
-# Predict using the generator
-def predict_with_generator(model, input_image):
-    prediction = model(input_image, training=False)
-    return prediction
-
-# Get prediction
-predicted_image = predict_with_generator(generator, new_image)
-
-# Display the result
-def display_results(input_image, generated_image, is_grayscale=True):
-    plt.figure(figsize=(10, 5))
-
-    plt.subplot(1, 2, 1)
-    plt.title('Input Image')
-    if is_grayscale:
-        plt.imshow(input_image[0, :, :, 0], cmap='gray')
-    else:
-        plt.imshow(input_image[0])
-    plt.axis('off')
-
-    plt.subplot(1, 2, 2)
-    plt.title('Generated Image')
-    if is_grayscale:
-        plt.imshow((generated_image[0, :, :, 0] + 1) / 2, cmap='gray')  # Adjust based on output range
-    else:
-        plt.imshow((generated_image[0] + 1) / 2)  # Adjust based on output range
-    plt.axis('off')
-
-    plt.show()
-
-# Display results
-display_results(new_image, predicted_image, is_grayscale=True)
+visualize_losses(gen_losses, disc_losses, val_gen_losses, val_psnrs)
